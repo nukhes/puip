97d185b798d7f7a4f166c4c5b5da4d4fc86ca561
nukhes <nukhes@protonmail.com>
2024-01-02 19:47:37 -0300

    source code :)

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Net.NetworkInformation;
+
+if (NetworkInterface.GetIsNetworkAvailable()) {
+  var https = new HttpClient();
+  string ip = await https.GetStringAsync("https://api.ipify.org");
+  Console.WriteLine($"\nYour public IP: {ip}");
+} else {
+  Console.WriteLine("You are not connected to the Internet!");
+}
+Console.ReadLine();
